7feda7cec8e4d47d82c22064bcbb41e2cba69f52
yongchengzhu <yongchengzhu002@gmail.com>
2023-11-13 20:03:01 -0500

    fix image ordering.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const getChapters = async (url) => fetchTextContents(url, 'chapter-name', getIma
 const getImages = async (url) => {
   const contentElement = document.querySelector('.content');
   contentElement.innerHTML = '';
+
   const html = await (await fetch(`/search?url=${url}`)).text();
 
   const createButton = (className, textContent, href) => {
@@ -20,20 +21,42 @@ const getImages = async (url) => {
     match && createButton(className, textContent, match[1]);
   };
 
-  const createImg = src => contentElement.appendChild(Object.assign(document.createElement('img'), { 
-    onload: () => URL.revokeObjectURL(src), src 
-  }));
-  
+  const createImg = async (src) => {
+    try {
+      const response = await fetch(`/image?url=${src}`);
+      if (response.ok) {
+        const blob = await response.blob();
+        const img = Object.assign(document.createElement('img'), {
+          onload: () => URL.revokeObjectURL(src),
+          src: URL.createObjectURL(blob),
+        });
+        contentElement.appendChild(img);
+        return img;
+      }
+    } catch (error) {
+      console.error(`Error fetching image: ${src}`, error);
+    }
+  };
+
   addButton('navi-change-chapter-btn-prev', 'Previous Chapter', /<a[^>]+class="[^"]*navi-change-chapter-btn-prev[^"]*"[^>]*href="([^"]+)"[^>]*>/);
   addButton('navi-change-chapter-btn-next', 'Next Chapter', /<a[^>]+class="[^"]*navi-change-chapter-btn-next[^"]*"[^>]*href="([^"]+)"[^>]*>/);
 
-  await Promise.all(Array.from(html.matchAll(/<img[^>]+src="([^"]+)"[^>]*>/g), async ({ 1: image }) => {
-    try {
-      const response = await fetch(`/image?url=${image}`);
-      if (response.ok) createImg(URL.createObjectURL(await response.blob()));
-    } catch (error) { console.error(`Error fetching image: ${image}`, error); }
-  }));
+  const matches = Array.from(html.matchAll(/<img[^>]+src="([^"]+)"[^>]*>/g));
+
+  // Map each image to a function that returns a promise
+  const imagePromises = matches.map(({ 1: image }) => () => createImg(image));
+
+  // Execute promises sequentially
+  for (const promiseFn of imagePromises) {
+    await promiseFn();
+  }
 };
+
+
+
+
+
+
 
 const fetchTextContents = async (url, className, handleClick) => {
   const contentElement = document.querySelector('.content');
