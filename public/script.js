e11bd327d6d684ec980cf4c375609b6cad74b7db
yongchengzhu <yongchengzhu002@gmail.com>
2023-11-15 23:59:54 -0500

    destroy content element on navigation.

--- public/script.js
+++ public/script.js
@@ -1,9 +1,7 @@
 const BASE_URL = "https://chapmanganato.com"
 
-const search = async () => {
-  const title = document.getElementById('textBox').value.replace(/\s+/g, '_');
-  navigateTo(`/search/story/${title}`);
-};
+const search = async () => 
+  navigateTo(`/search/story/${document.getElementById('textBox').value.replaceAll(' ', '_')}`);
 
 const navigateTo = path => {
   history.pushState(null, null, path);
@@ -11,39 +9,43 @@ const navigateTo = path => {
 };
 
 const handleNavigation = path => {
-  if (path.startsWith('/search/story/')) {
-    fetchTextContents(BASE_URL + path, 'item-title');
-  } else if (path.startsWith('/manga-') && path.includes('/chapter-')) {
-    getImages(BASE_URL + path);
-  } else if (path.startsWith('/manga-')) {
-    fetchTextContents(BASE_URL + path, 'chapter-name');
-  } else {
-    // Do nothing.
+  document.querySelector('.content') && document.querySelector('.content').remove();
+  document.body.appendChild(Object.assign(document.createElement('div'), { className: "content" }));
+  switch (true) {
+    case path.startsWith('/search/story/'):
+      fetchTextContents(BASE_URL + path, 'item-title');
+      break;
+    case path.startsWith('/manga-') && path.includes('/chapter-'):
+      getImages(BASE_URL + path);
+      break;
+    case path.startsWith('/manga-'):
+      fetchTextContents(BASE_URL + path, 'chapter-name');
+      break;
+    default:
+      break;
   }
-};
-
-const fetchTextContents = async (url, className) => {
-  const contentElement = document.querySelector('.content');
-  contentElement.innerHTML = '';
-  const html = await (await fetch(`/search?url=${url}`)).text();
-  Array.from(new DOMParser().parseFromString(html, 'text/html').querySelectorAll(`.${className}`))
-    .forEach(({ textContent, href }) => {
-      const node = contentElement.appendChild(Object.assign(document.createElement('div'), { textContent, className }));
-      node.addEventListener('click', () => navigateTo(new URL(href).pathname));
-    });
 };
 
 window.addEventListener('popstate', () => {
   handleNavigation(document.location.pathname);
 });
 
-const getImages = async (url) => {
-  const contentElement = document.querySelector('.content');
-  contentElement.innerHTML = '';
+const fetchTextContents = async (url, className) => {
+  const content = document.querySelector('.content');
   const html = await (await fetch(`/search?url=${url}`)).text();
-  document.querySelector('.content').appendChild(Object.assign(document.createElement('h1'), { textContent: (new DOMParser().parseFromString(html, 'text/html').querySelector('h1') || {}).textContent }));
+  Array.from(new DOMParser().parseFromString(html, 'text/html').querySelectorAll(`.${className}`))
+    .forEach(({ textContent, href }) => {
+      const node = content.appendChild(Object.assign(document.createElement('div'), { textContent, className }));
+      node.addEventListener('click', () => navigateTo(new URL(href).pathname));
+    });
+};
+
+const getImages = async (url) => {
+  const content = document.querySelector('.content');
+  const html = await (await fetch(`/search?url=${url}`)).text();
+  content.appendChild(Object.assign(document.createElement('h1'), { textContent: (new DOMParser().parseFromString(html, 'text/html').querySelector('h1') || {}).textContent }));
   const createButton = (className, textContent, href) => {
-    const button = contentElement.appendChild(Object.assign(document.createElement('button'), { textContent, className }));
+    const button = content.appendChild(Object.assign(document.createElement('button'), { textContent, className }));
     button.onclick = () => navigateTo(href);
   };
   const addButton = (className, textContent, regex) => {
@@ -58,7 +60,7 @@ const getImages = async (url) => {
         const img = Object.assign(document.createElement('img'), {
           src: '/images/' + imageName,
         });
-        contentElement.appendChild(img);
+        content.appendChild(img);
         return img;
       }
     } catch (error) {
