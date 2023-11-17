7dcf3f8dc497bcedfe8afd16b5fcaa49abca3161
yongchengzhu <yongchengzhu002@gmail.com>
2023-11-16 20:06:47 -0500

    clean up frontend.

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 const BASE_URL = "https://chapmanganato.com"
 
-const search = async () => 
-  navigateTo(`/search/story/${document.getElementById('textBox').value.replaceAll(' ', '_')}`);
+const search = async () => navigateTo(`/search/story/${document.getElementById('textBox').value.replaceAll(' ', '_')}`);
 
 const navigateTo = path => {
   history.pushState(null, null, path);
@@ -10,16 +9,17 @@ const navigateTo = path => {
 
 const handleNavigation = path => {
   document.querySelector('.content') && document.querySelector('.content').remove();
-  document.body.appendChild(Object.assign(document.createElement('div'), { className: "content" }));
+  const content = Object.assign(document.createElement('div'), { className: "content" });
+  document.body.appendChild(content);
   switch (true) {
     case path.startsWith('/search/story/'):
-      fetchTextContents(BASE_URL + path, 'item-title');
+      fetchTextContents(BASE_URL + path, 'item-title', content);
       break;
     case path.startsWith('/manga-') && path.includes('/chapter-'):
-      getImages(BASE_URL + path);
+      getImages(BASE_URL + path, content);
       break;
     case path.startsWith('/manga-'):
-      fetchTextContents(BASE_URL + path, 'chapter-name');
+      fetchTextContents(BASE_URL + path, 'chapter-name', content);
       break;
     default:
       break;
@@ -30,8 +30,7 @@ window.addEventListener('popstate', () => {
   handleNavigation(document.location.pathname);
 });
 
-const fetchTextContents = async (url, className) => {
-  const content = document.querySelector('.content');
+const fetchTextContents = async (url, className, content) => {
   const html = await (await fetch(`/search?url=${url}`)).text();
   Array.from(new DOMParser().parseFromString(html, 'text/html').querySelectorAll(`.${className}`))
     .forEach(({ textContent, href }) => {
@@ -40,46 +39,22 @@ const fetchTextContents = async (url, className) => {
     });
 };
 
-const getImages = async (url) => {
-  const content = document.querySelector('.content');
-  const html = await (await fetch(`/search?url=${url}`)).text();
-  content.appendChild(Object.assign(document.createElement('h1'), { textContent: (new DOMParser().parseFromString(html, 'text/html').querySelector('h1') || {}).textContent }));
-  const createButton = (className, textContent, href) => {
-    const button = content.appendChild(Object.assign(document.createElement('button'), { textContent, className }));
-    button.onclick = () => navigateTo(href);
-  };
-  const addButton = (className, textContent, regex) => {
-    const match = html.match(regex);
-    match && createButton(className, textContent, new URL(match[1]).pathname);
-  };
-  const createImg = async (src) => {
+const getImages = async (url, content) => {
+  const html = new DOMParser().parseFromString(await (await fetch(`/search?url=${url}`)).text(), 'text/html');
+  const addButtons = () => [html.querySelector('.navi-change-chapter-btn-prev'), html.querySelector('.navi-change-chapter-btn-next')]
+    .filter(Boolean)
+    .forEach(({ textContent, href }) => content.appendChild(Object.assign(document.createElement('button'), { textContent }))
+      .onclick = () => navigateTo(new URL(href).pathname));
+  content.appendChild(Object.assign(document.createElement('h1'), { textContent: (html.querySelector('h1') || {}).textContent }));
+  addButtons();
+  for (const { src } of Array.from(html.querySelectorAll('img'))) {
     try {
-      const response = await fetch(`/image?url=${src}`);
-      if (response.ok) {
-        const imageName = await response.text();
-        const img = Object.assign(document.createElement('img'), {
-          src: '/images/' + imageName,
-        });
-        content.appendChild(img);
-        return img;
-      }
+      content.appendChild(Object.assign(document.createElement('img'), { src: `/images/${await (await fetch(`/image?url=${src}`)).text()}` }));
     } catch (error) {
       console.error(`Error fetching image: ${src}`, error);
     }
-  };
-  const matches = Array.from(html.matchAll(/<img[^>]+src="([^"]+)"[^>]*>/g));
-  const imagePromises = matches.map(({ 1: image }) => () => createImg(image));
-  addButton('navi-change-chapter-btn-prev', 'Previous Chapter', /<a[^>]+class="[^"]*navi-change-chapter-btn-prev[^"]*"[^>]*href="([^"]+)"[^>]*>/);
-  addButton('navi-change-chapter-btn-next', 'Next Chapter', /<a[^>]+class="[^"]*navi-change-chapter-btn-next[^"]*"[^>]*href="([^"]+)"[^>]*>/);
-  for (const promiseFn of imagePromises) {
-    const startPerformance = performance.now();
-    await promiseFn();
-    const endPerformance = performance.now();
-    const executionTimePerformance = endPerformance - startPerformance;
-    console.log(`Execution time: ${executionTimePerformance} milliseconds`);
   }
-  addButton('navi-change-chapter-btn-prev', 'Previous Chapter', /<a[^>]+class="[^"]*navi-change-chapter-btn-prev[^"]*"[^>]*href="([^"]+)"[^>]*>/);
-  addButton('navi-change-chapter-btn-next', 'Next Chapter', /<a[^>]+class="[^"]*navi-change-chapter-btn-next[^"]*"[^>]*href="([^"]+)"[^>]*>/);
+  addButtons();
 };
 
 handleNavigation(document.location.pathname);
